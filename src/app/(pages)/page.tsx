4cfe7c8eb305ef9d913dd3e1ee2ce40528bda238
tuna2134 <hello@tuna2134.dev>
2025-09-02 02:33:02 +0000

    add

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -12,7 +12,14 @@ export default function Home() {
             <div className="py-8">
                 <h2 className="text-2xl font-bold mb-4">Mission</h2>
                 <p>
-                    本プロジェクトの役割は主にNeodylandの開発者たちにグローバルIPアドレスが付いたVMを無償で貸し出して、彼らの開発を支えることです。
+                    本プロジェクトの役割は主にNeodylandの開発者にグローバルIPアドレスが付いたVMを無償で貸し出して、彼らの開発を支えることです。
+                    さらにネットワークの研究を独自に進め、ブログなどで公開をする予定です。
+                </p>
+            </div>
+            <div className="py-8">
+                <h2 className="text-2xl font-bold mb-4">料金は？</h2>
+                <p>
+                    非営利で進めるため、無償で提供しています。しかし、金がかかるため、寄付を受け付けております。寄付金は主にネットワークの維持に使われます。
                 </p>
             </div>
         </>
